38f88472e0cdd98dc7f732580d8e246bb0e2934e
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2021-10-27 19:21:12 +0530

    More python practices

---
+++ enumeration.py
@@ -0,0 +1,7 @@
+
+baby = ["Samaira", "Cookie", "Samaj"]
+
+for index, item in enumerate(baby):
+    print(index, item)
+    if(index == 1):
+        print(f"{item} a dog")
---
+++ list-comprehension.py
@@ -0,0 +1,2 @@
+numbers = [num**2 for num in [1, 4, 5, 2, 6]] #num is getting each number of the list one by one and getting exponent by 2. The result list is getting stored in the numbers list
+print(numbers) # gives O/P [1, 16, 25, 4, 36]
