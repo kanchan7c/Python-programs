bade68d3d19fc15795a743ef6a70acd5fb068db8
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2021-10-28 16:06:59 +0530

    changes to project pokemon

--- pokemonAPI_project.py
+++ pokemonAPI_project.py
@@ -1,29 +1,27 @@
 import requests
 
-pokemon = input("Enter the Pokemon name:  ")
-pokemon = pokemon.lower()
+while True:
+    pokemon = input("Enter the Pokemon name:  ")
+    pokemon = pokemon.lower()
 
-dynamicURL = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+    dynamicURL = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+    req = requests.get(dynamicURL)
 
-req = requests.get(dynamicURL)
-pokemonData = req.json()
+    if req.status_code == 200:
+        pokemonData = req.json()
 
-print("\n","Name is:  ", pokemonData['name'], "\n")
+        print("\n","Name is:  ", pokemonData['name'], "\n")
 
-print ("ABILITIES: ", "\n")
-for ability in pokemonData['abilities']:
-    print(ability['ability']['name'], "\n")
+        print ("ABILITIES: ", "\n")
+        for ability in pokemonData['abilities']:
+            print(ability['ability']['name'], "\n")
 
-print ("More about ABILITIES: ", "\n")
-for ability in pokemonData['abilities']:
-    print(ability['ability']['url'], "\n")
+        print ("More about ABILITIES: ", "\n")
+        for ability in pokemonData['abilities']:
+            print(ability['ability']['url'], "\n")
 
-print ("More about GAME INDICES: ", "\n")
-for indices in pokemonData['game_indices']:
-    print("Game index:  ",indices['game_index'], "\n")
-    print("Version name: ", indices['version']['name'], "\n")
-    print("More on Version: ", indices['version']['url'], "\n")
-
+    else: 
+        print(f"'{pokemon}' is not a valid pokemon name")
 
 
 
